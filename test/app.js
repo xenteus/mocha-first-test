d37808e4fe91b133d006bbfa94dec2872865fe05
xenteus <37004095+xenteus@users.noreply.github.com>
2018-03-06 09:38:40 -0800

    Add files via upload

---
+++ test/app.js
@@ -0,0 +1,13 @@
+var assert = require('assert');
+describe('Math', function() {
+  describe('testone()', function() {
+    it('testing if 5+5 = 10', function(){
+      assert.equal(9, 3*3);
+    });
+  });
+  describe('testtwo()', function() {
+	it('testing if (2-2)*8 =0', function() {
+		assert.equal(0, (2-2)*8);
+	});
+  });
+});
